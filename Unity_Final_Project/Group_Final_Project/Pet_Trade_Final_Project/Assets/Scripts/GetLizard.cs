6c43e8fe605f6a493ffd236054af63418c2cd2b1
Brandon Castillo <brandoncastillo1030@gmail.com>
2020-05-10 18:37:45 -0400

    added scenes for dogs and lizard

---
+++ Unity_Final_Project/Group_Final_Project/Pet_Trade_Final_Project/Assets/Scripts/GetLizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GetLizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene("Pet Store Lizard");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene("Breeder Lizard");
+        }
+    }
+}
