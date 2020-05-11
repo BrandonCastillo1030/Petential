b10c4fd1c246f0a4018a6e4aed9b3752c76b21ba
Brandon Castillo <brandoncastillo1030@gmail.com>
2020-05-10 22:49:14 -0400

    Got the States

---
+++ Unity_Final_Project/Group_Final_Project/Pet_Trade_Final_Project/Assets/Scripts/StoryText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class StoryText : MonoBehaviour
+{
+    public Text textComponent;
+
+    public State StartingState;
+    public State Situation1;
+    public State Situation2;
+
+    State state;
+    void Start()
+    {
+        //starts the game with the starting state
+        state = StartingState;
+        //gets the states and the text
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
