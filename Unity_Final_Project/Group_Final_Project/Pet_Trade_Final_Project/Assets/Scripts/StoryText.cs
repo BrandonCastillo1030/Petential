5532adc51dce6b25c36d440a2285522102516d71
Brandon Castillo <brandoncastillo1030@gmail.com>
2020-05-17 19:03:26 -0400

    guh

--- Unity_Final_Project/Group_Final_Project/Pet_Trade_Final_Project/Assets/Scripts/StoryText.cs
+++ Unity_Final_Project/Group_Final_Project/Pet_Trade_Final_Project/Assets/Scripts/StoryText.cs
@@ -13,6 +13,36 @@ public class StoryText : MonoBehaviour
     public int health = 100;
     public int bored = 100;
 
+    public int foodNeo = 100;
+    public int waterNeo = 100;
+    public int cleanNeo = 100;
+    public int healthNeo = 100;
+    public int boredNeo = 100;
+
+    public int foodRico = 100;
+    public int waterRico = 100;
+    public int cleanRico = 100;
+    public int healthRico = 100;
+    public int boredRico = 100;
+
+    public int foodNed = 100;
+    public int waterNed = 100;
+    public int cleanNed = 100;
+    public int healthNed = 100;
+    public int boredNed = 100;
+
+    public int fooTed = 100;
+    public int waterTed = 100;
+    public int cleanTed = 100;
+    public int healthTed = 100;
+    public int boredTed = 100;
+
+    public int foodShred = 100;
+    public int waterShred = 100;
+    public int cleanShred = 100;
+    public int healthShred = 100;
+    public int boredShred = 100;
+
     public int decreaseStat = 0; 
 
     public int decreaseFood = 0;
@@ -28,6 +58,46 @@ public class StoryText : MonoBehaviour
     public State Situation4;
     public State Situation5;
     public State Situation6;
+
+    public State StartingStateLeo;
+    public State SituationLeo1;
+    public State SituationLeo2;
+    public State SituationLeo3;
+    public State SituationLeo4;
+    public State SituationLeo5;
+    public State SituationLeo6;
+
+    public State StartingStateRico;
+    public State SituationRico1;
+    public State SituationRico2;
+    public State SituationRico3;
+    public State SituationRico4;
+    public State SituationRico5;
+    public State SituationRico6;
+
+    public State StartingStateNed;
+    public State SituationNed1;
+    public State SituationNed2;
+    public State SituationNed3;
+    public State SituationNed4;
+    public State SituationNed5;
+    public State SituationNed6;
+
+    public State StartingStateTed;
+    public State SituationTed1;
+    public State SituationTed2;
+    public State SituationTed3;
+    public State SituationTed4;
+    public State SituationTed5;
+    public State SituationTed6;
+
+    public State StartingStateShred;
+    public State SituationShred1;
+    public State SituationShred2;
+    public State SituationShred3;
+    public State SituationShred4;
+    public State SituationShred5;
+    public State SituationShred6;
 
     public Text MoneyText;
     public Text FoodText;
@@ -116,7 +186,7 @@ public class StoryText : MonoBehaviour
         var nextStates = state.GetNextStates();
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
-        if (state == StartingState && sceneName == "Leo")
+        if (state == StartingState && sceneName == "Lizard")
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
@@ -129,6 +199,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 food = food + 10;
+                
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -249,22 +320,8 @@ public class StoryText : MonoBehaviour
                 }
             }
 
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
-
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
-            }
-
-
         }
-        else if (state == Situation1 && sceneName == "Leo")
+        else if (state == Situation1 && sceneName == "Lizard")
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
@@ -397,22 +454,9 @@ public class StoryText : MonoBehaviour
                 }
             }
 
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
-
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
-            }
-
-
+           
         }
-        else if (state == Situation2 && sceneName == "Leo")
+        else if (state == Situation2 && sceneName == "Lizard")
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
@@ -425,6 +469,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 food = food + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -458,6 +503,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 water = water + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -491,6 +537,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 clean = clean + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -523,6 +570,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 bored = bored + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -545,34 +593,23 @@ public class StoryText : MonoBehaviour
                 }
             }
 
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
-
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
-            }
-
-
         }
-        else if (state == Situation3 && sceneName == "Leo")
+        else if (state == Situation3 && sceneName == "Lizard")
         {
+
+            
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
                 state = Situation4;
 
-                decreaseFood = UnityEngine.Random.Range(5, 15);
+                decreaseFood = UnityEngine.Random.Range(5, 5);
                 decreaseWater = UnityEngine.Random.Range(5, 15);
                 decreaseClean = UnityEngine.Random.Range(5, 15);
                 decreaseBored = UnityEngine.Random.Range(5, 15);
-                decreaseStat = UnityEngine.Random.Range(1, 4);
+                decreaseStat = UnityEngine.Random.Range(1,1);
 
                 food = food + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -606,6 +643,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 water = water + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -639,6 +677,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 clean = clean + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -671,6 +710,7 @@ public class StoryText : MonoBehaviour
                 decreaseStat = UnityEngine.Random.Range(1, 4);
 
                 bored = bored + 10;
+                health = health - 25;
                 if (decreaseStat == 1)
                 {
                     food = food - decreaseFood;
@@ -693,26 +733,34 @@ public class StoryText : MonoBehaviour
                 }
             }
 
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
 
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
+        }
+        else if (state == Situation4 && sceneName == "Lizard")
+        {
+           
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                state = Situation5;
+            }
+        }
+        else if (state == Situation5 && sceneName == "Lizard")
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                health = health + 25;
+                state = Situation6;
+
             }
 
 
         }
-        else if (state == Situation4 && sceneName == "Leo")
+        else if (state == Situation6 && sceneName == "Lizard")
         {
+           
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                state = Situation5;
+                
 
                 decreaseFood = UnityEngine.Random.Range(5, 15);
                 decreaseWater = UnityEngine.Random.Range(5, 15);
@@ -745,7 +793,7 @@ public class StoryText : MonoBehaviour
             else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
 
-                state = Situation5;
+                
 
                 decreaseFood = UnityEngine.Random.Range(5, 15);
                 decreaseWater = UnityEngine.Random.Range(5, 15);
@@ -778,7 +826,7 @@ public class StoryText : MonoBehaviour
 
             else if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                state = Situation5;
+                
 
                 decreaseFood = UnityEngine.Random.Range(5, 15);
                 decreaseWater = UnityEngine.Random.Range(5, 15);
@@ -810,7 +858,7 @@ public class StoryText : MonoBehaviour
             }
             else if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                state = Situation5;
+                
 
                 decreaseFood = UnityEngine.Random.Range(5, 15);
                 decreaseWater = UnityEngine.Random.Range(5, 15);
@@ -840,168 +888,6 @@ public class StoryText : MonoBehaviour
                     Debug.Log(bored);
                 }
             }
-
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
-
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
-            }
-
-
-        }
-        else if (state == Situation5 && sceneName == "Leo")
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
-            {
-                state = Situation6;
-
-                decreaseFood = UnityEngine.Random.Range(5, 15);
-                decreaseWater = UnityEngine.Random.Range(5, 15);
-                decreaseClean = UnityEngine.Random.Range(5, 15);
-                decreaseBored = UnityEngine.Random.Range(5, 15);
-                decreaseStat = UnityEngine.Random.Range(1, 4);
-
-                food = food + 10;
-                if (decreaseStat == 1)
-                {
-                    food = food - decreaseFood;
-                    Debug.Log("Food Amount: " + food);
-                }
-                else if (decreaseStat == 2)
-                {
-                    water = water - decreaseWater;
-                    Debug.Log("Water : " + water);
-                }
-                else if (decreaseStat == 3)
-                {
-                    clean = clean - decreaseClean;
-                    Debug.Log("Clean :" + clean);
-                }
-                else if (decreaseStat == 4)
-                {
-                    bored = bored - decreaseBored;
-                    Debug.Log("Boredom: " + bored);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-
-                state = Situation6;
-
-                decreaseFood = UnityEngine.Random.Range(5, 15);
-                decreaseWater = UnityEngine.Random.Range(5, 15);
-                decreaseClean = UnityEngine.Random.Range(5, 15);
-                decreaseBored = UnityEngine.Random.Range(5, 15);
-                decreaseStat = UnityEngine.Random.Range(1, 4);
-
-                water = water + 10;
-                if (decreaseStat == 1)
-                {
-                    food = food - decreaseFood;
-                    Debug.Log(food);
-                }
-                else if (decreaseStat == 2)
-                {
-                    water = water - decreaseWater;
-                    Debug.Log(water);
-                }
-                else if (decreaseStat == 3)
-                {
-                    clean = clean - decreaseClean;
-                    Debug.Log(clean);
-                }
-                else if (decreaseStat == 4)
-                {
-                    bored = bored - decreaseBored;
-                    Debug.Log(bored);
-                }
-            }
-
-            else if (Input.GetKeyDown(KeyCode.Alpha3))
-            {
-                state = Situation6;
-
-                decreaseFood = UnityEngine.Random.Range(5, 15);
-                decreaseWater = UnityEngine.Random.Range(5, 15);
-                decreaseClean = UnityEngine.Random.Range(5, 15);
-                decreaseBored = UnityEngine.Random.Range(5, 15);
-                decreaseStat = UnityEngine.Random.Range(1, 4);
-
-                clean = clean + 10;
-                if (decreaseStat == 1)
-                {
-                    food = food - decreaseFood;
-                    Debug.Log(food);
-                }
-                else if (decreaseStat == 2)
-                {
-                    water = water - decreaseWater;
-                    Debug.Log(water);
-                }
-                else if (decreaseStat == 3)
-                {
-                    clean = clean - decreaseClean;
-                    Debug.Log(clean);
-                }
-                else if (decreaseStat == 4)
-                {
-                    bored = bored - decreaseBored;
-                    Debug.Log(bored);
-                }
-            }
-            else if (Input.GetKeyDown(KeyCode.Alpha4))
-            {
-                state = Situation6;
-
-                decreaseFood = UnityEngine.Random.Range(5, 15);
-                decreaseWater = UnityEngine.Random.Range(5, 15);
-                decreaseClean = UnityEngine.Random.Range(5, 15);
-                decreaseBored = UnityEngine.Random.Range(5, 15);
-                decreaseStat = UnityEngine.Random.Range(1, 4);
-
-                bored = bored + 10;
-                if (decreaseStat == 1)
-                {
-                    food = food - decreaseFood;
-                    Debug.Log(food);
-                }
-                else if (decreaseStat == 2)
-                {
-                    water = water - decreaseWater;
-                    Debug.Log(water);
-                }
-                else if (decreaseStat == 3)
-                {
-                    clean = clean - decreaseClean;
-                    Debug.Log(clean);
-                }
-                else if (decreaseStat == 4)
-                {
-                    bored = bored - decreaseBored;
-                    Debug.Log(bored);
-                }
-            }
-
-            else if (Input.GetKeyDown(KeyCode.Alpha5) && health <= 100)
-            {
-
-                if (health <= 50)
-                {
-                    SceneManager.LoadScene("BadVet");
-                }
-                else if (health > 50)
-                {
-                    SceneManager.LoadScene("GetBetterVet");
-                }
-            }
-
 
         }
 
